9ef682f9f7489e53e952ca5de9212920d11ed191
serhiichayck <chayck7@gmail.com>
2021-04-19 22:46:20 +0300

    Installed additional modules

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "bootstrap";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "animate.css/animate.css";
+
+import {
+    faShoppingCart,
+    faDollarSign
+} from "@fortawesome/free-solid-svg-icons";
 
 Vue.config.productionTip = false;
 
