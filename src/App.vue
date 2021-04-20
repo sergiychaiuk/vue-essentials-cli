ab4bb495a7ad9c683c6bd20ef099979fd5d13558
serhiichayck <chayck7@gmail.com>
2021-04-20 18:20:37 +0300

    Added navigation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app" class="container mt-5">
     <h1>My Shop</h1>
+    <navbar
+      :cart="cart"
+      :cartQty="cartQty"
+      :cartTotal="cartTotal"
+      @toggle="toggleSliderStatus"
+      @delete="deleteItem"
+    ></navbar>
     <price-slider
       :sliderStatus="sliderStatus"
       :maximum.sync="maximum"
@@ -16,18 +23,43 @@
 <script>
 import ProductList from "./components/ProductList.vue";
 import PriceSlider from "./components/PriceSlider.vue";
+import Navbar from "./components/Navbar.vue";
 
 export default {
   name: "App",
+  components: {
+    Navbar,
+    ProductList,
+    PriceSlider,
+  },
   data: function () {
     return {
-      sliderStatus: true,
+      sliderStatus: false,
       maximum: 99,
       products: null,
       cart: [],
     };
   },
+  computed: {
+    cartTotal: function () {
+      let sum = 0;
+      this.cart.forEach(function (item) {
+        sum = sum + item.product.price * item.qty;
+      });
+      return sum;
+    },
+    cartQty: function () {
+      let qty = 0;
+      this.cart.forEach(function (item) {
+        qty = qty + item.qty;
+      });
+      return qty;
+    },
+  },
   methods: {
+    toggleSliderStatus: function () {
+      this.sliderStatus = !this.sliderStatus;
+    },
     addItem: function (product) {
       let whichProduct;
       let existing = this.cart.filter(function (item, index) {
@@ -45,10 +77,13 @@ export default {
         this.cart.push({ product: product, qty: 1 });
       }
     },
-  },
-  components: {
-    ProductList,
-    PriceSlider,
+    deleteItem: function (id) {
+      if (this.cart[id].qty > 1) {
+        this.cart[id].qty--;
+      } else {
+        this.cart.splice(id, 1);
+      }
+    },
   },
   mounted: function () {
     fetch("https://hplussport.com/api/products/order/price")
---
+++ src/components/Navbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav class="navbar navbar-light fixed-top">
+    <div class="navbar-text ml-auto d-flex">
+      <button class="btn btn-sm btn-outline-success" @click="$emit('toggle')">
+        <i class="fas fa-dollar-sign"></i>
+        <font-awesome-icon icon="dollar-sign"></font-awesome-icon>
+      </button>
+      <div class="ml-2" v-if="cart.length > 0">
+        <button
+          class="btn btn-success btn-sm dropdown-toggle"
+          id="cartDropdown"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          <span class="badge badge-pill badge-light">{{ cartQty }}</span>
+          <font-awesome-icon
+            icon="shopping-cart"
+            class="mx-2"
+          ></font-awesome-icon>
+          <price :value="Number(cartTotal)"></price>
+        </button>
+        <div
+          class="dropdown-menu dropdown-menu-right"
+          aria-labelledby="cartDropdown"
+        >
+          <div v-for="(item, index) in cart" :key="index">
+            <div class="dropdown-item-text text-nowrap text-right">
+              <span
+                class="badge badge-pill badge-warning align-text-top mr-1"
+                >{{ item.qty }}</span
+              >
+              {{ item.product.name }}
+              <price :value="Number(item.qty * item.product.price)"></price>
+              <a
+                href="#"
+                @click.stop="$emit('delete', index)"
+                class="badge badge-danger text-white"
+                >-</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import Price from "./Price.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+export default {
+  name: "Navbar",
+  props: ["cart", "cartQty", "cartTotal"],
+  components: {
+    FontAwesomeIcon,
+    Price,
+  },
+};
+</script>
+
+<style scoped></style>
