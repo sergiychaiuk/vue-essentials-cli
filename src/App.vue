02d1d538d10bd891c7adf83c8ce484287768eaae
serhiichayck <chayck7@gmail.com>
2021-04-20 14:32:03 +0300

    Created a component

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,18 @@
       Take a look at our offerings below
     </p>
     <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+    <Price value="4.52"></Price>
   </div>
 </template>
 
 <script>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import Price from "./components/Price.vue";
 
 export default {
   name: "App",
   components: {
+    Price,
     FontAwesomeIcon,
   },
 };
---
+++ src/components/Price.vue
@@ -0,0 +1,28 @@
+<template>
+  <span>{{
+    prefix + Number.parseFloat(value * conversion).toFixed(precision)
+  }}</span>
+</template>
+
+<script>
+export default {
+  name: "Price",
+  props: {
+    value: Number,
+    prefix: {
+      type: String,
+      default: "$",
+    },
+    precision: {
+      type: Number,
+      default: 2,
+    },
+    conversion: {
+      type: Number,
+      default: 1,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
